1c5f93c90dc3ec105b81e3adac17a7ea308bdbf7
SilverBack Gorilla <silverbackgorilla@SilverBacks-MacBook-Pro.local>
2023-08-26 23:43:03 -0400

    fixed duplicates in add to cart

--- customers/urls.py
+++ customers/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('items/<int:pk>', views.SingleProductView.as_view(), name='single-item'),
     path('items/<int:product_id>/user/<int:user_id>/add-to-cart', views.AddToCartView.as_view(), name='add-to-cart'),
     path('users/<int:user_id>/cart/', views.UserCartView.as_view(), name='user-cart'),
-
 ]
